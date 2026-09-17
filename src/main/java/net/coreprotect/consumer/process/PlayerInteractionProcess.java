PlayPro/CoreProtect

Avoid getState calls in PlayerInteractListener to improve performance (fixes #984)

src/main/java/net/coreprotect/consumer/process/PlayerInteractionProcess.java
@@ -2,17 +2,17 @@ package net.coreprotect.consumer.process;
 
 import net.coreprotect.database.ConsumerWriteBatch;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.BlockState;
 
 import net.coreprotect.database.logger.PlayerInteractLogger;
 
 class PlayerInteractionProcess {
 
-    static void process(ConsumerWriteBatch preparedStmt, int batchCount, String user, Object object, Material type) {
-        if (object instanceof BlockState) {
-            BlockState block = (BlockState) object;
-            PlayerInteractLogger.log(preparedStmt, batchCount, user, block, type);
+    static void process(ConsumerWriteBatch preparedStmt, int batchCount, String user, Object object, Material type, String blockData) {
+        if (object instanceof Location) {
+            Location location = (Location) object;
+            PlayerInteractLogger.log(preparedStmt, batchCount, user, location, type, blockData);
         }
     }
 }

src/main/java/net/coreprotect/consumer/process/Process.java
@@ -307,7 +307,7 @@ public class Process {
                                     ContainerBreakProcess.process(writeBatch, i, processId, id, blockType, user, object);
                                     break;
                                 case Process.PLAYER_INTERACTION:
-                                    PlayerInteractionProcess.process(writeBatch, i, user, object, blockType);
+                                    PlayerInteractionProcess.process(writeBatch, i, user, object, blockType, (String) data[7]);
                                     break;
                                 case Process.CONTAINER_TRANSACTION:
                                     ContainerTransactionProcess.process(writeBatch, writeBatch, i, processId, id, blockType, forceData, user, object);

src/main/java/net/coreprotect/database/logger/PlayerInteractLogger.java
@@ -3,7 +3,6 @@ package net.coreprotect.database.logger;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Location;
-import org.bukkit.block.BlockState;
 
 import net.coreprotect.CoreProtect;
 import net.coreprotect.config.Config;
@@ -24,9 +23,8 @@ public class PlayerInteractLogger {
         throw new IllegalStateException("Database class");
     }
 
-    public static void log(ConsumerWriteBatch preparedStmt, int batchCount, String user, BlockState block, Material blockType) {
+    public static void log(ConsumerWriteBatch preparedStmt, int batchCount, String user, Location location, Material blockType, String blockData) {
         try {
-            String blockData = block.getBlockData().getAsString();
             String blockKey = BlockTypeUtils.getBlockDataKey(blockData);
             if (blockType != null && blockKey.length() > 0) {
                 Material blockDataType = MaterialUtils.getType(blockKey);
@@ -47,7 +45,7 @@ public class PlayerInteractLogger {
                 return;
             }
 
-            CoreProtectPreLogEvent event = new CoreProtectPreLogEvent(user, block.getLocation(), CoreProtectPreLogEvent.Action.PLAYER_INTERACTION, LookupActions.INTERACTION, blockType, null, null);
+            CoreProtectPreLogEvent event = new CoreProtectPreLogEvent(user, location.clone(), CoreProtectPreLogEvent.Action.PLAYER_INTERACTION, LookupActions.INTERACTION, blockType, null, null);
             if (Config.getGlobal().API_ENABLED && !Bukkit.isPrimaryThread()) {
                 CoreProtect.getInstance().getServer().getPluginManager().callEvent(event);
             }

CheekyNox/coreprotect-clickhouse

(cherry picked from commit ac20d00d6260ba4ca5c9d50565227bc83b4f8b75)

src/main/java/net/coreprotect/consumer/process/PlayerInteractionProcess.java
@@ -2,17 +2,17 @@ package net.coreprotect.consumer.process;
 
 import java.sql.PreparedStatement;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.BlockState;
 
 import net.coreprotect.database.logger.PlayerInteractLogger;
 
 class PlayerInteractionProcess {
 
-    static void process(PreparedStatement preparedStmt, int batchCount, String user, Object object, Material type) {
-        if (object instanceof BlockState) {
-            BlockState block = (BlockState) object;
-            PlayerInteractLogger.log(preparedStmt, batchCount, user, block, type);
+    static void process(PreparedStatement preparedStmt, int batchCount, String user, Object object, Material type, String blockData) {
+        if (object instanceof Location) {
+            Location location = (Location) object;
+            PlayerInteractLogger.log(preparedStmt, batchCount, user, location, type, blockData);
         }
     }
 }

src/main/java/net/coreprotect/consumer/process/Process.java
@@ -289,7 +289,7 @@ public class Process {
                                     ContainerBreakProcess.process(preparedStmtContainers, i, processId, id, blockType, user, object);
                                     break;
                                 case Process.PLAYER_INTERACTION:
-                                    PlayerInteractionProcess.process(preparedStmtBlocks, i, user, object, blockType);
+                                    PlayerInteractionProcess.process(preparedStmtBlocks, i, user, object, blockType, (String) data[7]);
                                     break;
                                 case Process.CONTAINER_TRANSACTION:
                                     ContainerTransactionProcess.process(preparedStmtContainers, preparedStmtItems, i, processId, id, blockType, forceData, user, object);

src/main/java/net/coreprotect/database/logger/PlayerInteractLogger.java
@@ -4,7 +4,6 @@ import java.sql.PreparedStatement;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Location;
-import org.bukkit.block.BlockState;
 
 import net.coreprotect.CoreProtect;
 import net.coreprotect.config.Config;
@@ -24,9 +23,8 @@ public class PlayerInteractLogger {
         throw new IllegalStateException("Database class");
     }
 
-    public static void log(PreparedStatement preparedStmt, int batchCount, String user, BlockState block, Material blockType) {
+    public static void log(PreparedStatement preparedStmt, int batchCount, String user, Location location, Material blockType, String blockData) {
         try {
-            String blockData = block.getBlockData().getAsString();
             String blockKey = BlockTypeUtils.getBlockDataKey(blockData);
             if (blockType != null && blockKey.length() > 0) {
                 Material blockDataType = MaterialUtils.getType(blockKey);
@@ -47,7 +45,7 @@ public class PlayerInteractLogger {
                 return;
             }
 
-            CoreProtectPreLogEvent event = new CoreProtectPreLogEvent(user, block.getLocation(), CoreProtectPreLogEvent.Action.PLAYER_INTERACTION, LookupActions.INTERACTION, blockType, null, null);
+            CoreProtectPreLogEvent event = new CoreProtectPreLogEvent(user, location.clone(), CoreProtectPreLogEvent.Action.PLAYER_INTERACTION, LookupActions.INTERACTION, blockType, null, null);
             if (Config.getGlobal().API_ENABLED && !Bukkit.isPrimaryThread()) {
                 CoreProtect.getInstance().getServer().getPluginManager().callEvent(event);
             }
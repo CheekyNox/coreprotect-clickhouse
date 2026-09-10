6d77c280ddfc8f8468ab01340e21d08d44059351
Intelli <contact@intelli.software>
2026-09-10 16:16:21 -0600

    Avoid getState calls in PlayerInteractListener to improve performance (fixes #984)
    (cherry picked from commit ac20d00d6260ba4ca5c9d50565227bc83b4f8b75)

--- src/main/java/net/coreprotect/consumer/process/PlayerInteractionProcess.java
+++ src/main/java/net/coreprotect/consumer/process/PlayerInteractionProcess.java
@@ -2,17 +2,17 @@ package net.coreprotect.consumer.process;
 
 import java.sql.PreparedStatement;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.BlockState;
 
 import net.coreprotect.database.logger.PlayerInteractLogger;
 
 class PlayerInteractionProcess {
 
-    static void process(PreparedStatement preparedStmt, int batchCount, String user, Object object, Material type) {
-        if (object instanceof BlockState) {
-            BlockState block = (BlockState) object;
-            PlayerInteractLogger.log(preparedStmt, batchCount, user, block, type);
+    static void process(PreparedStatement preparedStmt, int batchCount, String user, Object object, Material type, String blockData) {
+        if (object instanceof Location) {
+            Location location = (Location) object;
+            PlayerInteractLogger.log(preparedStmt, batchCount, user, location, type, blockData);
         }
     }
 }
